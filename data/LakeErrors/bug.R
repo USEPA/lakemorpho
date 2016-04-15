cdec57f07f613796b53f16b7419470869a36f9ff
Jeff Hollister <jeff.w.hollister@gmail.com>
2016-04-15 13:20:00 -0400

    remove length/width bug data

--- data/LakeErrors/bug.R
+++
@@ -1,9 +0,0 @@
-library(lakemorpho)
-library(rgdal)
-x <- readOGR("data/LakeErrors", "ErrorLakes")
-x1 <- lakeMorphoClass(x[2,],NULL,NULL,NULL)
-for(i in 1:100){
-  xl <- lakeMaxLength(x1,50)
-  print(i)
-  #plot(x1$maxWidthLine,add=T,col="red",lwd=3)
-}
