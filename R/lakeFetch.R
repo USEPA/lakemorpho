c3955d610156389f14432df7b56e4323292f6df2
jsta <stachel2@msu.edu>
2022-12-28 15:08:32 -0700

    add missing imports

--- R/lakeFetch.R
+++ R/lakeFetch.R
@@ -20,7 +20,7 @@
 #'             \href{http://edis.ifas.ufl.edu/pdffiles/FA/FA08100.pdf}{Link}
 #' 
 #' @import geosphere methods
-#' @importFrom sf st_as_sf st_transform st_crs st_bbox st_coordinates st_linestring st_sfc st_length st_within st_intersection
+#' @importFrom sf st_as_sf st_transform st_crs st_bbox st_coordinates st_linestring st_sfc st_length st_within st_intersection st_sf st_geometry
 #' 
 #' @export
 #' 
