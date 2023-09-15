6ca7944e32705a3a33b848afa248ddafbe9e836c
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2023-09-15 15:28:03 -0400

    added startup message.

--- R/zzz.r
+++ R/zzz.r
@@ -1,8 +1,12 @@
 op <- options()
 
-.onLoad <- function(libname, pkgname){
-  options("rgdal_show_exportToProj4_warnings"="thin")
-  invisible()
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+"lakemorpho v01.3.0 NOTE: Version 1.3.0 of 'lakemorpho' uses 'sf' for all vector
+processing.  The 'rgdal' and 'rgeos' pacakges have been removed.  'raster' is 
+still used for raster data handling.  The output lakeMorpho class no longer 
+contains `sp` objects.  In subsequent releases, 'lakemorpho' will use only 'sf' 
+and 'terra', so please plan accordingly.")
 }
 
 .onUnload <- function(libname, pkgname){
